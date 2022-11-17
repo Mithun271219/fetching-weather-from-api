ad01a8d1636faf044f193fcda31a298a335954c7
Mithun271219 <106835778+Mithun271219@users.noreply.github.com>
2022-11-17 13:08:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let res=fetch('https://restcountries.com/v2/all').then(data=>{return data.json()
     for (var i in data1){
         let cc=data1[i].name;
         let div=document.createElement('div')
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cc}&appid=ad5d0074b3b3b64e13c15d32311cf1da`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cc}&appid=31eaa08be873f89f89b6be82dd27c09f`)
         .then((dat2)=>{return dat2.json()})
         .then((data3)=>{
             let temp=data3.main.temp-273.1
-            disp=`Country: ${cc} and Temperature: ${temp.toFixed(3)} °C`
+            disp=`Country: ${cc} and the Temperature: ${temp.toFixed(3)} °C`
             div.innerHTML+=disp
             main.append(div)
         })
